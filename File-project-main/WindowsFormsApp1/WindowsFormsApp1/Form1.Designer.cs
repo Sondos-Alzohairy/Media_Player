53e92eb58ad4db7ed30662e26337f36a35edd6f5
Sondos-Alzohairy <124532598+Sondos-Alzohairy@users.noreply.github.com>
2024-04-26 22:04:53 +0300

    Add files via upload

---
+++ File-project-main/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_prev = new System.Windows.Forms.Button();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.track_list = new System.Windows.Forms.ListBox();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Repeat_fun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_prev
+            // 
+            this.btn_prev.Location = new System.Drawing.Point(289, 1);
+            this.btn_prev.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_prev.Name = "btn_prev";
+            this.btn_prev.Size = new System.Drawing.Size(69, 23);
+            this.btn_prev.TabIndex = 1;
+            this.btn_prev.Text = "Previos";
+            this.btn_prev.UseVisualStyleBackColor = true;
+            this.btn_prev.Click += new System.EventHandler(this.btn_prev_Click);
+            // 
+            // btn_open
+            // 
+            this.btn_open.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_open.Location = new System.Drawing.Point(1, 2);
+            this.btn_open.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(139, 23);
+            this.btn_open.TabIndex = 2;
+            this.btn_open.Text = "Open";
+            this.btn_open.UseVisualStyleBackColor = false;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(362, 2);
+            this.btn_next.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(64, 23);
+            this.btn_next.TabIndex = 3;
+            this.btn_next.Text = "Next";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.Location = new System.Drawing.Point(144, 1);
+            this.btn_pause.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(70, 23);
+            this.btn_pause.TabIndex = 4;
+            this.btn_pause.Text = "Pause";
+            this.btn_pause.UseVisualStyleBackColor = true;
+            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
+            // 
+            // btn_play
+            // 
+            this.btn_play.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_play.Location = new System.Drawing.Point(218, 1);
+            this.btn_play.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(67, 24);
+            this.btn_play.TabIndex = 5;
+            this.btn_play.Text = "Play";
+            this.btn_play.UseVisualStyleBackColor = false;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(430, 4);
+            this.btn_stop.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(70, 24);
+            this.btn_stop.TabIndex = 8;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // track_list
+            // 
+            this.track_list.FormattingEnabled = true;
+            this.track_list.HorizontalScrollbar = true;
+            this.track_list.Location = new System.Drawing.Point(827, 29);
+            this.track_list.Margin = new System.Windows.Forms.Padding(2);
+            this.track_list.Name = "track_list";
+            this.track_list.Size = new System.Drawing.Size(196, 329);
+            this.track_list.TabIndex = 6;
+            this.track_list.SelectedIndexChanged += new System.EventHandler(this.track_list_SelectedIndexChanged);
+            // 
+            // player
+            // 
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(1, 32);
+            this.player.Margin = new System.Windows.Forms.Padding(2);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(822, 326);
+            this.player.TabIndex = 0;
+            // 
+            // Repeat_fun
+            // 
+            this.Repeat_fun.Location = new System.Drawing.Point(505, 5);
+            this.Repeat_fun.Name = "Repeat_fun";
+            this.Repeat_fun.Size = new System.Drawing.Size(75, 23);
+            this.Repeat_fun.TabIndex = 9;
+            this.Repeat_fun.Text = "Repeat";
+            this.Repeat_fun.UseVisualStyleBackColor = true;
+            this.Repeat_fun.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 441);
+            this.Controls.Add(this.Repeat_fun);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.track_list);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.btn_pause);
+            this.Controls.Add(this.btn_prev);
+            this.Controls.Add(this.btn_play);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.Button btn_prev;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.ListBox track_list;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button Repeat_fun;
+    }
+}
---
+++ File-project-main/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            player.PlayStateChange -= Player_PlayStateChange;
+            player.PlayStateChange += Player_PlayStateChange;
+            isShuffleEnabled = true;
+
+        }
+        private bool isShuffleEnabled = false;
+        private bool isRepeatEnabled = false;
+        private string[] paths;// Array to store file paths  
+        private string[] files;// Array to store file names
+
+
+        // Event handler for opening files
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "Media Files|*.mpg;*.avi;*.wma;*.mov;*.wav;*.mp2;*.mp3;*.mp4;*.flv;*.m4a;*.mkv;|All Files|*.*"
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                files = ofd.SafeFileNames;
+                paths = ofd.FileNames;
+
+                track_list.Items.Clear();
+                for (int x = 0; x < files.Length; x++)
+                {
+                    track_list.Items.Add(files[x]);
+                }
+
+                // Shuffle playlist as soon as it's loaded if shuffle is enabled
+                if (isShuffleEnabled)
+                {
+                    ShufflePlaylist();
+                }
+            }
+        }
+
+        // Event handler for selecting a track from the list box
+        private void track_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (track_list.SelectedIndex >= 0 && track_list.SelectedIndex < paths.Length)
+            {
+                try
+                {
+                    // Play the selected track
+                    player.URL = paths[track_list.SelectedIndex];
+                    player.Ctlcontrols.play();
+
+                    
+
+                    // Unsubscribe from previous PlayStateChange event
+                    player.PlayStateChange -= Player_PlayStateChange;
+                    // Subscribe to the PlayStateChange event to handle track ending
+                    player.PlayStateChange += Player_PlayStateChange;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error playing the selected track: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Single Event handler for PlayStateChange event
+        private void Player_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            // Check if the current state is 'Media Ended'
+            if ((WMPPlayState)e.newState == WMPPlayState.wmppsMediaEnded)
+            {
+                // Decide the next track to play based on whether shuffle is enabled
+                int nextIndex = isShuffleEnabled ? GetNextShuffleIndex() : track_list.SelectedIndex + 1;
+
+                // Check if we've reached the end of the playlist
+                if (nextIndex >= track_list.Items.Count)
+                {
+                    if (isRepeatEnabled) // If repeat is enabled, either reshuffle or restart from the beginning
+                    {
+                        if (isShuffleEnabled)
+                            ShufflePlaylist(); // Reshuffle if shuffle is enabled
+                        nextIndex = 0; // Start from the beginning
+                    }
+                    else
+                    {
+                        return; // Stop playback or handle as needed
+                    }
+                }
+
+                // Set the next item as the current selection and play
+                track_list.SelectedIndex = nextIndex; // This will trigger 'track_list_SelectedIndexChanged' to play the next track
+            }
+        }
+
+        // Event handler for the Play button
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            player.Ctlcontrols.play();
+        }
+
+        // Event handler for the Pause button
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            player.Ctlcontrols.pause();
+        }
+
+        // Event handler for the Next button
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            if (track_list.SelectedIndex < track_list.Items.Count - 1)
+            {
+                track_list.SelectedIndex++;
+            }
+        }
+
+        // Event handler for the Previous button
+        private void btn_prev_Click(object sender, EventArgs e)
+        {
+            if (track_list.SelectedIndex > 0)
+            {
+                track_list.SelectedIndex--;
+            }
+        }
+
+        // Event handler for the volume control
+       
+
+        // Event handler for the Stop button
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            player.Ctlcontrols.stop();
+        }
+        private int GetNextShuffleIndex()
+        {
+            // Implement shuffle logic here to determine the next index
+            // For now, just proceed linearly as an example
+            return track_list.SelectedIndex + 1;
+        }
+        private void ShufflePlaylist()
+        {
+            Random rnd = new Random();
+            int n = track_list.Items.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+
+                var value = track_list.Items[k];
+                track_list.Items[k] = track_list.Items[n];
+                track_list.Items[n] = value;
+
+                // Swap paths as well to keep track_list and paths in sync
+                string tempPath = paths[k];
+                paths[k] = paths[n];
+                paths[n] = tempPath;
+            }
+
+            track_list.SelectedIndex = 0; // Automatically play the first song in the shuffled playlist
+        }
+
+
+        // Event handler for PlayStateChange event modified for repeat option
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private bool isRepeated = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isRepeatEnabled = !isRepeatEnabled; // Toggle the repeat state
+
+            // Update button text to reflect the new state
+            // Make sure the button is correctly named; update "button1" to match the actual name
+            Repeat_fun.Text = isRepeatEnabled ? "Repeat: ON" : "Repeat: OFF";
+        }
+    }
+}
